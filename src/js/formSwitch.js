efb03e5de02a4f37a92191a5384037ecfcbc0a3b
NEPotato <james.boydens@gmail.com>
2025-07-16 10:00:43 +0200

    Genral_dispaly_fix

--- src/js/formSwitch.js
+++ src/js/formSwitch.js
@@ -2,10 +2,10 @@ import { mailform, checkinForm } from "./variables";
 
 
 if (window.location.pathname === '/checkin') {
-  checkinForm.classList.add('active'); // Ensure the checkin form is active
+  checkinForm.classList.add('is-active'); // Ensure the checkin form is active
 }
 
 if (window.location.pathname === '/checkout') {
-  checkinForm.classList.remove('active'); // Ensure the checkin form is not active
-  checkinForm.classList.add('inactive'); // Optionally add an inactive class
+  checkinForm.classList.remove('is-active'); // Ensure the checkin form is not active
+  checkinForm.classList.add('is-inactive'); // Optionally add an inactive class
 }
