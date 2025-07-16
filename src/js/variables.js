9994f974611df1b6bcd2b8cb88c51ce1b746bd12
NEPotato <james.boydens@gmail.com>
2025-07-16 11:33:49 +0200

    setup_api_url_local

--- src/js/variables.js
+++ src/js/variables.js
@@ -1,6 +1,11 @@
+//get url localhost
+const url = window.location.hostname;
 const checkinForm = document.querySelector('#checkin');
 const checkoutForm = document.querySelector('#checkout');
 const mailForm = document.querySelector('#completion');
-const baseApiUrl = 'http://localhost/presence/cms/api/content/'; // Replace with your API URL
+const baseApiUrl = url==='localhost'?
+'http://localhost/presence/cms/api/content/':'https://ingrwf12.cepegra-frontend.xyz/cockpit_james/api/content/';
+//sets api url if localhost
+console.log('baseApiUrl', baseApiUrl);
 
 export { checkinForm, checkoutForm, mailForm, baseApiUrl };
